a735bb0989aa95dd8bae9fa69b95f795d407b1e4
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2024-01-02 07:46:36 +0000

    complete auth add config

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,5 +1,4 @@
 import conf from "../conf/conf";
-
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
@@ -12,22 +11,46 @@ export class AuthService {
       .setProject(conf.appwriteProjectId);
     this.account = new Account(this.client);
   }
-    
+
   async createAccount({ email, password, name }) {
     try {
-      const userAccount = await this.account.create(ID.unique, email, password, name);
+      const userAccount = await this.account.create(
+        ID.unique,
+        email,
+        password,
+        name
+      );
       if (userAccount) {
-        return userAccount;
+        return this.login({ email, password });
       } else {
-        throw new Error("Error creating account");
+        return userAccount;
       }
     } catch (error) {
-      throw
-      error
+      throw error;
     }
   }
-  
+  async login({ email, password }) {
+    try {
+      return await this.client.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser :: error", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: logout :: error", error);
+    }
+  }
 }
-
 const authService = new AuthService();
 export default AuthService;
