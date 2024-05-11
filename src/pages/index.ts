69c039a3bfedfc443abd97c2842ddca1863b0b01
mr.gaurav9554 <mr.gaurav9554@gmail.com>
2024-05-11 23:23:33 +0000

    add all pages

---
+++ src/pages/index.ts
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import AddPost from "./AddPost";
+import Login from "./Login";
+import Signup from "./Signup";
+import EditPost from "./EditPost";
+import Post from "./Post";
+import AllPosts from "./AllPosts";
+
+export { Home, AddPost, Login, Signup, EditPost, Post, AllPosts };
